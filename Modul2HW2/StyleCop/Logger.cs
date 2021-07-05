9c74f640187d38d954f37a90155ebcc121919a3f
CtrlAltBear <ctrl_alt_bear@protonmail.com>
2021-07-05 21:18:23 +0300

    no message

---
+++ Modul2HW2/StyleCop/Logger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace StyleCop
+{
+    internal class Logger
+    {
+        private static Logger _user = new Logger();
+        private StringBuilder _logs;
+
+        static Logger()
+        {
+        }
+
+        private Logger()
+        {
+            _logs = new StringBuilder();
+        }
+
+        public static Logger User
+        {
+            get
+            {
+                return _user;
+            }
+        }
+
+        public void Write(LogType type, string message)
+        {
+            var log = $"{DateTime.UtcNow}: {type.ToString()}: {message}";
+            _logs.AppendLine(log);
+            Console.WriteLine(log);
+        }
+
+        public override string ToString()
+        {
+            return _logs.ToString();
+        }
+    }
+}
